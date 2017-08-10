05ff0cc8a5092953a2454c2bc0615996f85b1dd2
sppandlkk <u900222@gmail.com>
2017-08-09 20:26:43 -0500

    use github data

--- ControlChart/app.R
+++ ControlChart/app.R
@@ -7,8 +7,8 @@ library(DT)
 
 ### read in data
 link <- "https://raw.githubusercontent.com/sppandlkk/control_chart/master/data/episode_data.csv"
-#dataUse <- read.csv(url(link))
-dataUse <- read.csv("../data/episode_data.csv")
+dataUse <- read.csv(url(link))
+
 
 ### deal with data
 dataUse$procDate <- as.Date(dataUse$procDate, "%Y-%m-%d")
